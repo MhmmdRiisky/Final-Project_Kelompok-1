c66cb5cba5b79f3e5d140b38b38b54087bd4051c
Fatih Mubasyir <114373275+fathbsyr@users.noreply.github.com>
2025-06-23 08:36:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 Hello from Flask App on Kubernetes with Jenkins! test-1"
+    return "🚀 Hello from Flask App on Kubernetes with Jenkins! INI KELOMPOK-1"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
